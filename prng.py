ae7d7b9a74283dd4773af363bf027650c4059146
16ozcoldbru <osborna2@oregonstate.edu>
2022-10-01 09:33:28 -0700

    Initial commit

---
+++ prng.py
@@ -0,0 +1,11 @@
+# Name: Andrew Osborne
+# OSU Email: osborna2@oregonstate.edu
+# Course: CS361
+# Assignment: Microservices Warm-up
+# Due Date: 10/3/2022
+# Description: An exploration of microservices
+
+import random, time
+while True:
+    time.sleep(3)
+    print(random.randint(1, 10))
